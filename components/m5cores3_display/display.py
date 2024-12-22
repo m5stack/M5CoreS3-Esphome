402ba5611fa959358e78fe376f947eef5ff30458
Henrique Cassus <henriquecassus@gmail.com>
2024-12-22 17:58:14 +0100

    Update display.py

--- components/m5cores3_display/display.py
+++ components/m5cores3_display/display.py
@@ -111,7 +111,7 @@ def _validate(config):
 
 
 CONFIG_SCHEMA = cv.All(
-    font.validate_pillow_installed,
+    #font.validate_pillow_installed,
     display.FULL_DISPLAY_SCHEMA.extend(
         {
             cv.GenerateID(): cv.declare_id(ILI9XXXDisplay),
