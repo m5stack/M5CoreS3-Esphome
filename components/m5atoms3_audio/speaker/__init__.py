8c1dc16f7434c354189ebb05ef743cab2282e853
pullen <pullen@pullenit.com>
2025-10-17 14:41:23 +0000

    Add sample rate configuration to I2SAudioSpeaker and update schema

--- components/m5atoms3_audio/speaker/__init__.py
+++ components/m5atoms3_audio/speaker/__init__.py
@@ -13,6 +13,7 @@ from .. import (
 
 CONF_BUFFER_SIZE = "buffer_size"
 CONF_DMA_BUF_COUNT = "buffer_count"
+CONF_SAMPLE_RATE = "sample_rate"
 
 
 CODEOWNERS = ["@jesserockz"]
@@ -29,7 +30,7 @@ CONFIG_SCHEMA = speaker.SPEAKER_SCHEMA.extend(
         cv.GenerateID(CONF_I2S_AUDIO_ID): cv.use_id(I2SAudioComponent),
         cv.Optional(CONF_BUFFER_SIZE, default=1024): cv.int_range(min=256, max=2048),
         cv.Optional(CONF_DMA_BUF_COUNT, default=8): cv.int_range(min=2, max=16),
-
+        cv.Optional(CONF_SAMPLE_RATE, default=16000): cv.int_range(min=8000, max=48000),
     }
 )
 
@@ -45,4 +46,5 @@ async def to_code(config):
         cg.add(var.set_buffer_size(config[CONF_BUFFER_SIZE]))
     if config[CONF_DMA_BUF_COUNT]:
         cg.add(var.set_buffer_size(config[CONF_DMA_BUF_COUNT]))
-   
+    if config[CONF_SAMPLE_RATE]:
+        cg.add(var.set_sample_rate(config[CONF_SAMPLE_RATE]))
